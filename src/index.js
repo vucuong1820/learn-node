7cfd3d4adc28e603ed048ae1b790976255458535
Manh Cuong <cuongvm@insights.is>
2022-04-25 21:31:31 +0700

    refractor routes and controllers

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ const morgan = require('morgan')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+
 
 //http logger
 app.use(morgan('combined'))
@@ -17,9 +23,8 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/home', (req, res) => {
-  res.render('home')
-})
+// routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
